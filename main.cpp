eb7e94decae74d675ab7092a5c00bc91553c6040
Dima Chistyakov <dimach.98.ru@gmail.com>
2018-06-04 14:52:22 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+#define  M  10
+
+
+int mas[M];
+
+int main()
+{
+    int i,ch,p;
+    for (i = 0; i < M; i++)
+    {
+        mas[i] = i + 1;
+    }
+    cin >> ch;
+
+
+    do
+    {
+        p = M / 2;
+        if (ch <= mas[p])
+        {
+            p=p-p/2;
+            if(ch==p)
+            {
+                break;
+            }
+        }
+        else
+        {
+            p=p+p/2;
+            if(ch==p)
+            {
+                break;
+            }
+        }
+    } while (i=999);
+
+
+/*
+	for (i = 0; i < M; i++)
+	{
+		cout << mas[i] << " ";
+	}
+*/
+
+    system("pause");
+    return 0;
+}
