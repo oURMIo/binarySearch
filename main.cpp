50a6fb3fddea41f7eff56eaa0ab389349be96a36
Dima Chistyakov <dimach.98.ru@gmail.com>
2018-06-13 10:39:03 +0300

    last_vershen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
@@ -8,47 +7,37 @@ using namespace std;
 
 int mas[M];
 // test VCS
-int main()
-{
-    int i,ch,p=M,exs=0;
-    for (i = 0; i < M; i++)
-    {
+int main() {
+    int i, ch, p = M, exs = 0;
+    for (i = 0; i < M; i++) {
         mas[i] = 100 + i + 1;
     }
 
-	for (i = 0; i < M; i++)
-	{
-		cout << mas[i] << " ";
-	}
-	cout<<endl<<"введи число : ";
+    for (i = 0; i < M; i++) {
+        cout << mas[i] << " ";
+    }
+    cout << endl << "введи число : ";
 
     cin >> ch;
-    p = p/ 2;
-    do
-    {
-        if (ch <= mas[p])
-        {
-            if(ch==mas[p] || ch+1==mas[p] || ch-1==mas[p])
-            {
-                cout<<""<<mas[p-1]<<"    and     "<<mas[p+1];
+    p = p / 2;
+    do {
+        if (ch <= mas[p]) {
+            if (ch == mas[p] || ch + 1 == mas[p] || ch - 1 == mas[p]) {
+                cout << "" << mas[p - 1] << "    and     " << mas[p + 1];
                 return 0;
 
             }
-            p=p-p/2;
-        }
-        else
-        {
-            if(ch==mas[p]|| ch+1==mas[p] || ch-1==mas[p])
-            {
-                cout<<""<<mas[p-1]<<"    and     "<<mas[p+1];
+            p = p - p / 2;
+        } else {
+            if (ch == mas[p] || ch + 1 == mas[p] || ch - 1 == mas[p]) {
+                cout << "" << mas[p - 1] << "    and     " << mas[p + 1];
                 return 0;
 
             }
-            p=p+p/2;
+            p = p + p / 2;
         }
-    } while (exs=1);
+    } while (exs = 1);
 
-
+}
 //    system("pause");
 //    return 0;
-}
